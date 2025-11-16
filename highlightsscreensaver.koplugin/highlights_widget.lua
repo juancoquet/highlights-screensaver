164d752132e148653de2beb97b24286c76b3a746
juancoquet <juancoquet@gmail.com>
2025-11-16 22:17:18 +0000

    feat: add hardcoded clipping file and refactor widget building with new fonts
    Signed-off-by: juancoquet <juancoquet@gmail.com>

--- highlightsscreensaver.koplugin/highlights_widget.lua
+++ highlightsscreensaver.koplugin/highlights_widget.lua
@@ -18,29 +18,22 @@ local M = {}
 function M.buildHighlightsScreensaverWidget(clipping)
 	local col_fg, col_bg = Blitbuffer.COLOR_WHITE, Blitbuffer.COLOR_BLACK
 	local width = Screen:getWidth() * 0.90
-	local font_size_main = 48
-	local function fontSizeAlt()
-		return math.ceil(font_size_main * 0.75)
-	end
+	local font_size_base = 48
 	local font_name_quote = "SourceSerif4-BoldIt.ttf"
-  local font_name_author = "SourceSerif4-Regular.ttf"
-  local font_name_note = "SourceSerif4-Bold.ttf"
+	font_name_quote = "NotoSerif-BoldItalic.ttf"
+	local font_name_author = "SourceSerif4-Regular.ttf"
+	font_name_author = "NotoSerif-Regular.ttf"
+	local font_name_note = "SourceSerif4-Bold.ttf"
+	font_name_note = "NotoSerif-Bold.ttf"
 
-	local highlight_text = TextBoxWidget:new({
-		text = clipping.text,
-		face = Font:getFace(font_name_quote, font_size_main),
-		width = width,
-		alignment = "left",
-		justified = true,
-		line_height = 0.5,
-		fgcolor = col_fg,
-		bgcolor = col_bg,
-	})
-	while highlight_text:getSize().h > Screen:getHeight() * 0.8 do
-		font_size_main = font_size_main - 2
-		highlight_text = TextBoxWidget:new({
+	local function buildWidget(base_font_size)
+		local function fontSizeAlt()
+			return math.ceil(base_font_size * 0.75)
+		end
+
+		local highlight_text = TextBoxWidget:new({
 			text = clipping.text,
-			face = Font:getFace(font_name_quote, font_size_main),
+			face = Font:getFace(font_name_quote, font_size_base),
 			width = width,
 			alignment = "left",
 			justified = true,
@@ -48,75 +41,91 @@ function M.buildHighlightsScreensaverWidget(clipping)
 			fgcolor = col_fg,
 			bgcolor = col_bg,
 		})
-	end
-	local left_border = LineWidget:new({
-		dimen = Geom:new({
-			w = Size.border.thick,
-			h = highlight_text:getSize().h,
-		}),
-		background = col_fg,
-	})
-	local highlight_group = HorizontalGroup:new({
-		left_border,
-		HorizontalSpan:new({ width = 20 }),
-		highlight_text,
-	})
-
-	local author_suffix = ""
-	if clipping.source_author and clipping.source_author ~= "" then
-		author_suffix = ", " .. clipping.source_author
-	end
-
-	local source_text = TextBoxWidget:new({
-		text = "— " .. clipping.source_title .. author_suffix,
-		face = Font:getFace(font_name_author, fontSizeAlt()),
-		width = width,
-		fgcolor = col_fg,
-		bgcolor = col_bg,
-		alignment = "left",
-	})
-
-	local content = VerticalGroup:new({
-		highlight_group,
-		VerticalSpan:new({ width = 24 }),
-		source_text,
-	})
-
-	if clipping.note and clipping.note ~= "" then
-		local separator = LineWidget:new({
+		while highlight_text:getSize().h > Screen:getHeight() * 0.8 do
+			font_size_base = font_size_base - 2
+			highlight_text = TextBoxWidget:new({
+				text = clipping.text,
+				face = Font:getFace(font_name_quote, font_size_base),
+				width = width,
+				alignment = "left",
+				justified = true,
+				line_height = 0.5,
+				fgcolor = col_fg,
+				bgcolor = col_bg,
+			})
+		end
+		local left_border = LineWidget:new({
 			dimen = Geom:new({
-				w = width,
-				h = Size.line.thin,
+				w = Size.border.thick,
+				h = highlight_text:getSize().h,
 			}),
 			background = col_fg,
 		})
-		local note_text = TextBoxWidget:new({
-			text = clipping.note,
-			face = Font:getFace(font_name_note, fontSizeAlt()),
+		local highlight_group = HorizontalGroup:new({
+			left_border,
+			HorizontalSpan:new({ width = 20 }),
+			highlight_text,
+		})
+
+		local author_suffix = ""
+		if clipping.source_author and clipping.source_author ~= "" then
+			author_suffix = ", " .. clipping.source_author
+		end
+
+		local source_text = TextBoxWidget:new({
+			text = "— " .. clipping.source_title .. author_suffix,
+			face = Font:getFace(font_name_author, fontSizeAlt()),
 			width = width,
 			fgcolor = col_fg,
 			bgcolor = col_bg,
 			alignment = "left",
-			line_height = 0.4,
 		})
-		table.insert(content, VerticalSpan:new({ width = 32 }))
-		table.insert(content, separator)
-		table.insert(content, VerticalSpan:new({ width = 24 }))
-		table.insert(content, note_text)
+
+		local content = VerticalGroup:new({
+			highlight_group,
+			VerticalSpan:new({ width = 24 }),
+			source_text,
+		})
+
+		if clipping.note and clipping.note ~= "" then
+			local separator = LineWidget:new({
+				dimen = Geom:new({
+					w = width,
+					h = Size.line.thin,
+				}),
+				background = col_fg,
+			})
+			local note_text = TextBoxWidget:new({
+				text = clipping.note,
+				face = Font:getFace(font_name_note, fontSizeAlt()),
+				width = width,
+				fgcolor = col_fg,
+				bgcolor = col_bg,
+				alignment = "left",
+				line_height = 0.4,
+			})
+			table.insert(content, VerticalSpan:new({ width = 32 }))
+			table.insert(content, separator)
+			table.insert(content, VerticalSpan:new({ width = 24 }))
+			table.insert(content, note_text)
+		end
+
+		local container = CenterContainer:new({
+			dimen = Screen:getSize(),
+			content,
+			padding = 0,
+			margin = 0,
+			bgcolor = col_bg,
+		})
+		local screensaver_widget = ScreenSaverWidget:new({
+			widget = container,
+			background = col_bg,
+			covers_fullscreen = true,
+		})
+		return screensaver_widget
 	end
 
-	local container = CenterContainer:new({
-		dimen = Screen:getSize(),
-		content,
-		padding = 0,
-		margin = 0,
-		bgcolor = col_bg,
-	})
-	local screensaver_widget = ScreenSaverWidget:new({
-		widget = container,
-		background = col_bg,
-		covers_fullscreen = true,
-	})
+	local screensaver_widget = buildWidget(font_size_base)
 
 	return screensaver_widget
 end
